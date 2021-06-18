425f2b90e2c9f93144301a0c4c49b4c4817219fa
shangruqi <shangruqi@turingvideo.net>
2021-06-18 18:31:33 +0800

    First commit, filter by text

---
+++ plugin/alc.vim
@@ -0,0 +1,23 @@
+"python <<EOF
+"print('alc worked with python')
+"EOF
+
+let s:plugin_root_dir = fnamemodify(resolve(expand('<sfile>:p')), ':h')
+
+python << EOF
+import sys
+from os.path import normpath, join
+import vim
+plugin_root_dir = vim.eval('s:plugin_root_dir')
+python_root_dir = normpath(join(plugin_root_dir, '..', 'python'))
+sys.path.insert(0, python_root_dir)
+
+from alc import main as alc_main
+#help(vim)
+EOF
+
+function! Alc(...)
+    python alc_main(*vim.eval('a:000'))
+endfunction
+command! -nargs=+ Alc call Alc(<f-args>)
+
---
+++ python/alc.py
@@ -0,0 +1,103 @@
+from functools import wraps
+import re
+
+import vim
+
+
+def temp_modify(proc):
+    @wraps(proc)
+    def _proc_modify(*args, **kwargs):
+        vim.command(":set modifiable")
+        ret = proc(*args, **kwargs)
+        vim.command(":set nomodifiable")
+        return ret
+    return _proc_modify
+
+
+_origins = {}
+
+
+def get_origin(buffer):
+    return _origins.get(buffer.number)
+
+
+def set_origin(buffer, origin):
+    _origins[buffer.number] = origin
+
+
+def del_origin(buffer):
+    if buffer in _origins:
+        _origins.pop(buffer.number)
+
+
+def main(cmd, *args):
+    vim.command(':set hidden')
+    # check if working as a view
+    onum = get_origin(vim.current.buffer)
+    if onum is not None:
+        print('This is a alc view')
+        # if it is, the origin may have been deleted
+        if onum in (b.number for b in vim.buffers):
+            pass
+        else:
+            print('It\'s origin is removed')
+            vim.command('bw')
+    else:
+        print('This is not a alc view, create one and copy contents')
+        contents = list(vim.current.buffer)
+        onum = vim.current.buffer.number
+        vim.command(':enew')
+        cnum = vim.current.buffer.number
+        set_origin(vim.current.buffer, onum)
+
+        # copy from origin to here
+        vim.current.buffer[:] = contents
+
+    if cmd == 'ftag':
+        cmd_ftag(*args)
+
+    else:
+        print('Unrecognized alc command {}'.format(cmd))
+
+
+_filter_out_tags = set()
+
+
+@temp_modify
+def cmd_ftag(*args):
+    if len(args) == 0:
+        print(sorted(_filter_out_tags))
+        return
+
+    def ftagcmds():
+        for arg in args:
+            for seg in arg.split(','):
+                yield seg
+
+    old_fot = set(_filter_out_tags)
+
+    for seg in ftagcmds():
+        if seg.endswith('-'):
+            tag = seg[:-1]
+            _filter_out_tags.add(tag)
+        elif seg.endswith('+'):
+            tag = seg[:-1]
+            if tag in _filter_out_tags:
+                _filter_out_tags.remove(tag)
+        else:
+            print('<tag>+ for show lines with tag or <tag>- for hide lines with tag')
+
+    if old_fot != _filter_out_tags:
+        pass
+
+    basecontents = list(vim.buffers[get_origin(vim.current.buffer)])
+    curcontents = vim.current.buffer
+    newcontents = []
+    ptn = re.compile('|'.join(sorted(_filter_out_tags)))
+    for line in basecontents:
+        if not ptn.search(line):
+            newcontents.append(line)
+
+    if len(newcontents) != len(curcontents):
+        vim.current.buffer[:] = newcontents
+
